860d28f33b8066cd158813312be4283c156fd631
Nathi <nathi@muzmatch.com>
2019-12-12 21:20:59 +0000

    add item model

---
+++ database/migrations/2019_12_12_194337_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('category_id')->unsigned()->index();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('description');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
